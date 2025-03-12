943a389ce5cf1cdbf405e336a73f05b6eb028986
Cody De Arkland <codydearkland@gmail.com>
2025-03-11 23:16:31 -0700

    removing database configs

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -14,7 +14,7 @@ export async function GET() {
     const { db } = await initDb();
     
     // BREAK-THIS: Ha - I've sabotaged you with bad queries
-    const result = await db.all(sql`SELECT * FROM products`);
+    const result = await db.all(sql`SELECT * FROM product`);
     
     const products = result.map((row: any) => ({
       id: row.id,
